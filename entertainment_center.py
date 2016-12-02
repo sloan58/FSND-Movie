b1e4557ea250a42312173c54d43d6081902e4e6e
Martin Sloan <martinsloan@Martins-MBP.home>
2016-12-02 07:36:11 -0500

    made updates per Udacity review feedback

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,40 @@
 import media
 import fresh_tomatoes
 
+# Create our movie_data details list.
 movie_data = [
-    ["No Retreat, No Surrender", "Train with the ghost of Bruce Lee", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwMDYyMDU4MV5BMl5BanBnXkFtZTcwOTUwNTYyMQ@@._V1_.jpg", "https://www.youtube.com/watch?v=4zYgAGt3bps"],
-    ["Rocky III", "Mr T!", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyOTYzMDMzMF5BMl5BanBnXkFtZTcwMTkzODM1NA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg", "https://www.youtube.com/watch?v=gbRDCWKqvEc"],
-    ["Top Gun", "The Top Gun Naval Fighter Weapons School", "https://upload.wikimedia.org/wikipedia/en/4/46/Top_Gun_Movie.jpg", "https://www.youtube.com/watch?v=qAfbp3YX9F0"],
+    [
+        "No Retreat, No Surrender",
+        "Train with the ghost of Bruce Lee",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwMDYyMDU4MV5BMl5BanBnXkFtZTcwOTUwNTYyMQ@@._V1_.jpg",
+        "https://www.youtube.com/watch?v=4zYgAGt3bps"
+    ],
+    [
+        "Rocky III",
+        "Mr T!",
+        # The next url is too long per PEP8 but I think it's okay in this case...
+        # https://www.python.org/dev/peps/pep-0008/#a-foolish-consistency-is-the-hobgoblin-of-little-minds
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyOTYzMDMzMF5BMl5BanBnXkFtZTcwMTkzODM1NA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+        "https://www.youtube.com/watch?v=gbRDCWKqvEc"
+    ],
+    [
+        "Top Gun",
+        "The Top Gun Naval Fighter Weapons School",
+        "https://upload.wikimedia.org/wikipedia/en/4/46/Top_Gun_Movie.jpg",
+        "https://www.youtube.com/watch?v=qAfbp3YX9F0"
+    ],
 
 ]
+
+# Create an empty list to hold Movie objects.
 movie_objects = []
 
+# Iterate movie_data and create new Movie objects.
 for movie in movie_data:
+
+    # Create a new Movie object and append it to movie_objects.
     movie_objects.append(media.Movie(movie[0], movie[1], movie[2], movie[3]))
 
+# Uses list of movie instances as input to generate an HTML file
+# and open it in the browser.
 fresh_tomatoes.open_movies_page(movie_objects)
--- media.py
+++ media.py
@@ -1,13 +1,10 @@
-import webbrowser
 
 
 class Movie:
-
+    """The Movie class accepts a movie title, storyline, poster_image_url and trailer_youtube_url as arguments
+    to create a new Movie object."""
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer)
